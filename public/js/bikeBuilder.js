1cc4d170d300f25ff0da9c1bfe7fddf1370e2124
Noah Seslar Britton <noahseslar@gmail.com>
2022-04-26 12:44:55 -0400

    final push

---
+++ public/js/bikeBuilder.js
@@ -0,0 +1,11 @@
+const mainImage = document.querySelector("#bikeBuilderImg")
+
+function wheel1() {
+  mainImage.src = "img/frame_wheel_1.png"
+}
+function wheel2() {
+  mainImage.src = "img/frame_wheel_2.png"
+}
+function reset() {
+  mainImage.src = "img/frame_no_wheel.png"
+}
